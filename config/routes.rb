b0460e82492d4e53c7d2fabddd84e2f2674e1a78
Colin <colintklein@gmail.com>
2020-07-20 13:45:37 -0500

    re-adds route

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
   namespace :api do
     get "/pages" => "pages#index"
+    get "/people" => "people#index"
     get "/examples" => "examples#index"
   end
 end
